d6660a0ce79960f69fe656a5a7018b00a5a51c7e
jwerzowa <52651471+jwerzowa@users.noreply.github.com>
2026-07-07 13:05:35 -0600

    Basic RCC get clock frequency function

--- drivers/src/rcc.c
+++ drivers/src/rcc.c
@@ -41,14 +41,14 @@ typedef struct {
     volatile uint32_t RCC_DCKCFGR;
     volatile uint32_t RCC_CKGATENR;
     volatile uint32_t RCC_DCKCFGR2;
-} RCC;
+} RCC_TypeDef;
 
 //RCC base Address
-#define RCC ((RCC*) 0x40023800)
-
+#define RCC ((RCC_TypeDef*) 0x40023800)
 
 //Function to set the frequency of the clock
-int RCC_Config(uint32_t frequency) {
+//Return enum RCC_OK = 0 if successful, RCC_Error = 1 if not 
+RCC_Status RCC_Config(uint32_t frequency) {
     // Enable HSI
     RCC->RCC_CR |= (1<<0); 
     // Wait for the HSI to be ready (Per reference manual: internal high-speed clock ready flag Set by hardware to indicate that the HSI oscillator is stable. After the HSION bit is cleared, HSIRDY goes low after 6 HSI clock cycles)
@@ -79,9 +79,23 @@ int RCC_Config(uint32_t frequency) {
     RCC->RCC_CFGR &= ~(0x7 << 13);  // clear PPRE2
     RCC->RCC_CFGR |=  (4 << 13);    // APB2 /2
 
+    // Now switch the system clock to PLL
     RCC->RCC_CFGR &= ~(3 << 0);
     RCC->RCC_CFGR |= (2 << 0);
 
-    while (!(RCC->RCC_CFGR & (2<<2)));
+    // Wait for the system clock to switch to PLL, take RCC_CFGR value and & it with (3 << 2) to get the SWS bits, check if they are equal to (2 << 2) which means PLL is selected as system clock
+    while ((RCC->RCC_CFGR & (3 << 2)) != (2 << 2));
+
+    return RCC_OK;
+}
+
+uint32_t RCC_GetSystemClockFrequency(void) {
+
+    switch (RCC->RCC_CFGR & (3<<2) >> 2) {
+        case 0: return 16000000;   // HSI
+        case 1: return 8000000;    // HSE (external crystal, assume 8MHz)
+        case 2: return 180000000;  // PLL
+        default: return 0;
+    }
 
 }
