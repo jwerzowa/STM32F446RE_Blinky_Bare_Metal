53c13e711df7b0dac13fc28b2b06808dac3c7184
jwerzowa <52651471+jwerzowa@users.noreply.github.com>
2026-07-07 15:47:16 -0600

    moved macros to .h files for use across .c files, beginning of the GPIO driver

--- drivers/src/gpio.c
+++ drivers/src/gpio.c
@@ -1,3 +1,32 @@
 
 
 //Instantiate all the addresses, make a structure for each GPIO port, and then make a pointer to each structure.
+
+#include "../inc/gpio.h"
+#include "../inc/rcc.h"
+#include <stdint.h>
+
+void GPIO_Init(GPIO_TypeDef* port, GPIO_PinConfig* config) {
+    
+    //Enable the clock for the GPIO port, set the appropriate bit based on port address, given the offsets are consistently 0x400 apart, we can calculate the bit position by subtracting the base address of GPIOA from the port address and dividing by 0x400.
+    RCC->RCC_AHB1ENR |= (1 << ((uint32_t)port - (uint32_t)GPIOA) / 0x400);
+    
+    //force the CPU to wait for the bus to complete the previous write before continuing
+    volatile uint32_t dummy = RCC->RCC_AHB1ENR;
+    (void)dummy;
+
+
+    //Set the mode, output type, speed, and pull-up or pull-down configuration for the specified pin.
+    port->MODER &= ~(3 << config->pin *2);
+    port->MODER |= (config->mode << config->pin *2);
+
+    port->OTYPER &= ~(1 << config->pin);
+    port->OTYPER |= (config->output_type << config->pin);
+
+    port->OSPEEDR &= ~(3 << config->pin *2);
+    port->OSPEEDR |= (config->speed << config->pin *2);
+
+    port->PUPDR &= ~(3 << config->pin *2);
+    port->PUPDR |= (config->pull << config->pin *2);
+
+}
